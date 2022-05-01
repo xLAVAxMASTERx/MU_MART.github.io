5f72dbf77f6211131761f7389e6af6a2990b63a8
xLAVAxMASTERx <93382134+xLAVAxMASTERx@users.noreply.github.com>
2022-05-01 13:12:27 +0530

    third commit

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/xLAVAxMASTERx/MU_MART.github.io";
+const repoURL = "https://github.com/xLAVAxMASTERx/MU_MART.github.io.git";
 
 ghpages.publish(
   pathname,
